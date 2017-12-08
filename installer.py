42946099a95a468ab8e26ff0dcd2baf54f62f500
Adrijaned <adrijaned@gmail.com>
2017-12-08 21:53:01 +0100

    v 1.1 - added changelog

--- installer.py
+++ installer.py
@@ -10,7 +10,7 @@ import time
 from urllib import request as url
 
 # Version of installer
-version = "1.0"
+version = "1.1"
 # Root URL to download files from
 root_url = "https://www.mirc.cz/"
 # List of things to download
@@ -25,11 +25,19 @@ new_profiles = []
 debug = False
 # Open logfile
 lof_file = open(file="log.txt", mode="at")
+# Changelist
+changelist = """
+V 1.1:
+    * added changelist
+    * logging now writes newlines
+    * I'm not exactly sure why it worked before, but now it is, as far as i 
+        know, working, AKA forge extracts into the right directory
+"""
 
 
 def log(message):
     print(message)
-    lof_file.write(message)
+    lof_file.write(message + "\n")
     lof_file.flush()
 
 
@@ -39,7 +47,12 @@ def print_debug(message="debug"):
 
 
 def accept(acceptTo):
-    """A little method to get whether user accepts something. Returns boolean value."""
+    """
+    Get whether user accepted to something.
+
+    :param acceptTo: Question to ask user
+    :return: Boolean - True if user accepted, False otherwise
+    """
     temp = input(acceptTo + " Y/N...")
     if temp == 'y' or temp == 'Y':
         return True
@@ -52,7 +65,8 @@ def accept(acceptTo):
 
 def print_makedirs_error(extended=""):
     """Prints default error message on os.makedirs()  fault. 'extended' is meant for extra info"""
-    log("Zdá se, že nelze vytvořit potřebnou složku. Může to být způsobeno například " +
+    log(
+        "Zdá se, že nelze vytvořit potřebnou složku. Může to být způsobeno například " +
         "nedostatečnými privilegii.\nZkontrolujte, že spouštíte tento instalátor s " +
         "právy dostačujícími pro zápis do zadané instalační lokace, a zkuste spustit tento " +
         "instalátor znovu.\nV případě přetrvávajících problémů neváhejte někoho kontaktovat " +
@@ -65,12 +79,14 @@ def urlretrievehook(in1, in2, in3):
     """Outputs progress of downloads"""
     if in1 * in2 >= in3:
         # Hack - add so many whitespace characters
-        log("Staženo 100 % z '" + currentFile + "', celková velikost: " + str(in3) + " B"
-                                                                                     "                  ")
+        log("Staženo 100 % z '" + currentFile + "', celková velikost: " + str(
+            in3) + " B"
+                   "                  ")
     else:
         temp_toPrint = "Staženo " + str(int(in1 * in2 / (in3 / 100))) + " % (" + \
-                       str(in1 * in2) + " B) z '" + currentFile + "', " + "celková velikost: " + \
-                       str(in3) + " B"
+                       str(
+                           in1 * in2) + " B) z '" + currentFile + "', " + \
+                       "celková velikost: " + str(in3) + " B"
         print(temp_toPrint, end="")
         for x in range(len(temp_toPrint)):
             print("\b", end="")
@@ -82,8 +98,10 @@ def addProfiles(data):
         if "profiles" not in data:
             data["profiles"] = {}
         data["profiles"][profile["name"]] = {u'gameDir': u'' + profile["dir"],
-                                             u'name': u'' + profile["name"] + '',
-                                             u'lastVersionId': u'' + profile["forge"]}
+                                             u'name': u'' + profile[
+                                                 "name"] + '',
+                                             u'lastVersionId': u'' + profile[
+                                                 "forge"]}
         outfile = open(file=mainDir + "launcher_profiles.json", mode="w")
         json.dump(obj=data, fp=outfile, sort_keys=True, indent=4)
     log("Hotovo")
@@ -96,10 +114,13 @@ def main():
     global mainDir  # For use in addProfiles()
 
     # Greeting
-    log("Vítejte v Linux Instalátoru RebelGames.net! (Adrijaned, v" + version + ")")
+    log(
+        "Vítejte v Linux Instalátoru RebelGames.net! (Adrijaned, v" + version +
+        ")")
 
     # Get installation root.
-    temp_bool = accept("Přejete si použít výchozí instalační lokaci '~/.minecraft/'?")
+    temp_bool = accept(
+        "Přejete si použít výchozí instalační lokaci '~/.minecraft/'?")
     if temp_bool:
         mainDir = "~/.minecraft/"
     else:
@@ -111,14 +132,17 @@ def main():
 
     # Check for root to exist. If not, attemp to create
     if not os.path.isdir(mainDir):
-        temp_bool = accept("Složka " + mainDir + " neexistuje, přejete si ji vytvořit? ")
+        temp_bool = accept(
+            "Složka " + mainDir + " neexistuje, přejete si ji vytvořit? ")
         if temp_bool:
             try:
                 os.makedirs(mainDir)
             except OSError:
                 print_makedirs_error()
         else:
-            log("Vytvořte prosím cílovou složku manuálně nebo zadejte instalaci do jiné lokace")
+            log(
+                "Vytvořte prosím cílovou složku manuálně nebo zadejte instalaci"
+                " do jiné lokace")
             time.sleep(3)
             exit(0)
     print_debug("2")
@@ -145,15 +169,19 @@ def main():
     # Get output from executing "java -version" (from stderr - because yellow fluffy rabbit)
     # Decode the output (b"java version...") to string using utf-8
     # Get version number or "not found"
-    java = subprocess.check_output(["java", "-version"], stderr=subprocess.STDOUT).decode("utf-8")
-    java_version = "not found" if java.find('"1.') == -1 else java[java.find('"1.') + 3]
+    java = subprocess.check_output(["java", "-version"],
+                                   stderr=subprocess.STDOUT).decode("utf-8")
+    java_version = "not found" if java.find('"1.') == -1 else java[
+        java.find('"1.') + 3]
     # Žaves - accepted java versions - if this installer lasts long enough to be more java
     # versions released, just add those here
-    accepted_java_versions = ["7", "8", "9", "10"]
+    accepted_java_versions = ["7", "8", "9", "10", "11"]
     if java_version not in accepted_java_versions:
-        log("Vaše verze javy(" + java + ") je zastaralá a proto modpacky RebelGames.net " +
+        log(
+            "Vaše verze javy(" + java + ") je zastaralá a proto modpacky RebelGames.net " +
             "nemusí být plně funkční.")
-        log("Prosím aktualizujte svou javu na novější verzi na 'https://java.com/'.")
+        log(
+            "Prosím aktualizujte svou javu na novější verzi na 'https://java.com/'.")
     print_debug("5")
 
     # Download config
@@ -164,7 +192,8 @@ def main():
                         reporthook=urlretrievehook)
     # Except nearly anything - there so many things that may go wrong and no way to handle them
     except:
-        log("Nepodařilo se stáhnout konfiguraci instalátoru, zkontrolujte své internetové "
+        log(
+            "Nepodařilo se stáhnout konfiguraci instalátoru, zkontrolujte své internetové "
             "připojení a v případě přetrvávajících obtíží prosím nahlašte bug na "
             "'https://forum.rebelgames.net/")
         time.sleep(3)
@@ -175,7 +204,8 @@ def main():
 
     # Do install forge + forgelibs?
     if accept("Chcete nainstalovat Forge? "):
-        toDownload.append({"path": "versions/", "item": "forge", "modpack": False})
+        toDownload.append(
+            {"path": "versions/", "item": "forge", "modpack": False})
     if accept("Chcete nainstalovat knihovny Forge? "):
         toDownload.append({"path": "", "item": "libs", "modpack": False})
     print_debug("7")
@@ -184,7 +214,8 @@ def main():
     config.read(tempDir + "config.ini")
     for section in config.sections():
         if ("rg" not in section) and (("dev" not in section) or devVersions):
-            if accept("Chcete nainstalovat modpack " + config.get(section, "description") + "?"):
+            if accept("Chcete nainstalovat modpack " + config.get(section,
+                                                                  "description") + "?"):
                 toDownload.append({"path": "", "item": section, "modpack": True,
                                    "desc": config.get(section, "description"),
                                    "forge": config.get(section, "forge")})
@@ -199,9 +230,10 @@ def main():
                             filename=tempDir + ".minecraft/" + currentFile,
                             reporthook=urlretrievehook)
         except:
-            log("Nepodařilo se stáhnout " + option["desc"] + " zkontrolujte své internetové "
-                                                             "připojení a v případě přetrvávajících obtíží prosím nahlašte bug na "
-                                                             "'https://forum.rebelgames.net/")
+            log("Nepodařilo se stáhnout " + option[
+                "desc"] + " zkontrolujte své internetové "
+                          "připojení a v případě přetrvávajících obtíží prosím nahlašte bug na "
+                          "'https://forum.rebelgames.net/")
             time.sleep(3)
             exit(1)
     print_debug("9")
@@ -214,8 +246,10 @@ def main():
             new_profiles.append({"name": "[RG] " + option["desc"],
                                  "forge": option["forge"],
                                  "dir": mainDir + option["item"] + "/"})
-        tempFile = tarfile.open(name=tempDir + ".minecraft/" + option["item"] + ".tar.gz")
-        tempFile.extractall(path=mainDir + option["path"] + option["item"])
+        tempFile = tarfile.open(
+            name=tempDir + ".minecraft/" + option["item"] + ".tar.gz")
+        tempFile.extractall(path=mainDir + option["path"] + (option["item"] if
+        not option["item"] == "forge" else ""))
         tempFile.close()
     print_debug("toDownload = " + str(toDownload))
     print_debug("10")
@@ -228,9 +262,11 @@ def main():
             data = json.load(json_file)
             addProfiles(data=data)
         except ValueError:
-            log("Neplatný JSON. Zkuste odstranit soubor '§INSTALLDIR§/launcher_profiles.json'.")
+            log(
+                "Neplatný JSON. Zkuste odstranit soubor '§INSTALLDIR§/launcher_profiles.json'.")
             exit(1)
-    elif accept("Nebyl nalezen soubor 'launcher_profiles.json'. Přejete si jej vytvořit? "):
+    elif accept(
+            "Nebyl nalezen soubor 'launcher_profiles.json'. Přejete si jej vytvořit? "):
         log("Vytváření profilů...")
         data = {}
         data["authenticationDatabase"] = {}
@@ -238,7 +274,7 @@ def main():
     print_debug("11")
 
     # Remove temporal directories
-    shutil.rmtree(tempDir)
+    #shutil.rmtree(tempDir)
     log("Instalace hotova")
     time.sleep(3)
     print_debug("END")
